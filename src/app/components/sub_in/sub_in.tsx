3f1256e939c79a247513f3fd5de0c1c7a165c780
Mengistie Hailemariam <menge456123@gmail.com>
2024-12-30 18:04:15 -0500

    done initial move to tailwind

--- src/app/components/sub_in/sub_in.tsx
+++ src/app/components/sub_in/sub_in.tsx
@@ -1,11 +1,10 @@
 'use client'
 import React, { useState, useRef, useEffect } from "react";
-import styles from "@/app/components/sub_in/page.module.css";
 
 export default function sub_in() {
     return(
         <main className="flex flex-col justify-between items-center min-h-screen p-24">
-        <div className="p-5 border-8 border-solid border-blue-700 rounded-3xl">
+        <div className="p-5 border-10 border-solid border-blue-700 rounded-3xl">
             <div className="grid grid-cols-8 grid-rows-4 gap-2.5">
                 
 
@@ -14,42 +13,42 @@ export default function sub_in() {
 
                 {/* 1 x 2 */}
                 <div className="bg-blue-700 w-24 h-24">
-                    <p className={styles.text}>Monday</p>
+                    <p className="text-center capitalize font-bold leading-12">Monday</p>
                 </div>
 
                 {/* 1 x 3 */}
                 <div className="bg-blue-700 w-24 h-24">
-                    <p className={styles.text}>Tuesday</p>
+                    <p className="text-center capitalize font-bold leading-12">Tuesday</p>
                 </div>
 
                 {/* 1 x 4 */}
                 <div className="bg-blue-700 w-24 h-24">
-                    <p className={styles.text}>Wednesday</p>
+                    <p className="text-center capitalize font-bold leading-12">Wednesday</p>
                 </div>
 
                 {/* 1 x 5 */}
                 <div className="bg-blue-700 w-24 h-24">
-                    <p className={styles.text}>Thursday</p>
+                    <p className="text-center capitalize font-bold leading-12">Thursday</p>
                 </div>
 
                 {/* 1 x 6 */}
                 <div className="bg-blue-700 w-24 h-24">
-                    <p className={styles.text}>Friday</p>
+                    <p className="text-center capitalize font-bold leading-12">Friday</p>
                 </div>
 
                 {/* 1 x 7 */}
                 <div className="bg-blue-700 w-24 h-24">
-                    <p className={styles.text}>Saturday</p>
+                    <p className="text-center capitalize font-bold leading-12">Saturday</p>
                 </div>
 
                 {/* 1 x 8 */}
                 <div className="bg-blue-700 w-24 h-24">
-                    <p className={styles.text}>Sunday</p>
+                    <p className="text-center capitalize font-bold leading-12">Sunday</p>
                 </div>
 
                 {/* 2 x 1 */}
-                <div className="bg-blue-700 w-24 h-24">
-                    <div className={styles.second_text}>9:00PM - 11:00PM</div>
+                <div className="flex bg-blue-700 w-24 h-24">
+                    <div className="text-center capitalize font-bold leading-14 transform translate-y-4">9:00PM - 11:00PM</div>
                 </div>
 
                 {/* 2 x 2 */}
@@ -88,8 +87,8 @@ export default function sub_in() {
                 </div>
 
                 {/* 3 x 1 */}
-                <div className="bg-blue-700 w-24 h-24">
-                    <div className={styles.second_text}>10:15PM - 12:15AM</div>
+                <div className="flex bg-blue-700 w-24 h-24">
+                    <div className="transform text-center capitalize font-bold leading-14 translate-y-4">10:15PM - 12:15AM</div>
                 </div>
 
                 {/* 3 x 2 */}
@@ -128,8 +127,8 @@ export default function sub_in() {
                 </div>
 
                 {/* 4 x 1 */}
-                <div className="bg-blue-700 w-24 h-24">
-                    <div className={styles.second_text}>12:15 PM - 2:15 AM</div>
+                <div className="flex bg-blue-700 w-24 h-24">
+                    <div className="transform text-center capitalize font-bold leading-14 translate-y-4">12:15 PM - 2:15 AM</div>
                 </div>
 
                 {/* 4 x 2 */}
--- src/app/components/sub_out/sub_out.tsx
+++ src/app/components/sub_out/sub_out.tsx
@@ -1,10 +1,9 @@
 'use client'
 import React, { useState, useRef, useEffect } from "react";
-import styles from "@/app/components/sub_out/page.module.css";
 export default function sub_out() {
     return(
         <main className="flex flex-col justify-between items-center min-h-screen p-24">
-        <div className="p-5 border-8 border-solid border-red-700 rounded-3xl">
+        <div className="p-5 border-10 border-solid border-red-700 rounded-3xl">
             <div className="grid grid-cols-8 grid-rows-4 gap-2.5">
                 
 
@@ -13,42 +12,42 @@ export default function sub_out() {
 
                 {/* 1 x 2 */}
                 <div className="bg-red-700 w-24 h-24">
-                    <p className={styles.text}>Monday</p>
+                    <p className="text-center capitalize font-bold leading-12">Monday</p>
                 </div>
 
                 {/* 1 x 3 */}
                 <div className="bg-red-700 w-24 h-24">
-                    <p className={styles.text}>Tuesday</p>
+                    <p className="text-center capitalize font-bold leading-12">Tuesday</p>
                 </div>
 
                 {/* 1 x 4 */}
                 <div className="bg-red-700 w-24 h-24">
-                    <p className={styles.text}>Wednesday</p>
+                    <p className="text-center capitalize font-bold leading-12">Wednesday</p>
                 </div>
 
                 {/* 1 x 5 */}
                 <div className="bg-red-700 w-24 h-24">
-                    <p className={styles.text}>Thursday</p>
+                    <p className="text-center capitalize font-bold leading-12">Thursday</p>
                 </div>
 
                 {/* 1 x 6 */}
                 <div className="bg-red-700 w-24 h-24">
-                    <p className={styles.text}>Friday</p>
+                    <p className="text-center capitalize font-bold leading-12">Friday</p>
                 </div>
 
                 {/* 1 x 7 */}
                 <div className="bg-red-700 w-24 h-24">
-                    <p className={styles.text}>Saturday</p>
+                    <p className="text-center capitalize font-bold leading-12">Saturday</p>
                 </div>
 
                 {/* 1 x 8 */}
                 <div className="bg-red-700 w-24 h-24">
-                    <p className={styles.text}>Sunday</p>
+                    <p className="text-center capitalize font-bold leading-12">Sunday</p>
                 </div>
 
                 {/* 2 x 1 */}
-                <div className="bg-red-700 w-24 h-24">
-                    <div className={styles.second_text}>9:00PM - 11:00PM</div>
+                <div className="flex bg-red-700 w-24 h-24">
+                    <div className="text-center capitalize font-bold leading-14 transform translate-y-4">9:00PM - 11:00PM</div>
                 </div>
 
                 {/* 2 x 2 */}
@@ -87,8 +86,8 @@ export default function sub_out() {
                 </div>
 
                 {/* 3 x 1 */}
-                <div className="bg-red-700 w-24 h-24">
-                    <div className={styles.second_text}>10:15PM - 12:15AM</div>
+                <div className="flex bg-red-700 w-24 h-24">
+                    <div className="transform text-center capitalize font-bold leading-14 translate-y-4">10:15PM - 12:15AM</div>
                 </div>
 
                 {/* 3 x 2 */}
@@ -127,8 +126,8 @@ export default function sub_out() {
                 </div>
 
                 {/* 4 x 1 */}
-                <div className="bg-red-700 w-24 h-24">
-                    <div className={styles.second_text}>12:15 PM - 2:15 AM</div>
+                <div className="flex bg-red-700 w-24 h-24">
+                    <div className="transform text-center capitalize font-bold leading-14 translate-y-4">12:15 PM - 2:15 AM</div>
                 </div>
 
                 {/* 4 x 2 */}
@@ -168,7 +167,5 @@ export default function sub_out() {
             </div>
         </div>
     </main>
- 
     )
-
 }
