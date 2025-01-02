4662d00560b046306e31090a5cd76e755b020419
Mengistie Hailemariam <menge456123@gmail.com>
2025-01-01 22:18:42 -0500

    i don't even know

--- src/app/components/header_button.tsx
+++ src/app/components/header_button.tsx
@@ -20,13 +20,13 @@ export const Component = () => {
                 <div className ="flex justify-center w-screen fixed bg-neon-yellow">
                     <h1 className="fixed left-2 top-3">Safewalk</h1>
                     <Link href= "sub-in">
-                        <div className="bg-blue-700 text-base rounded-md border-2 border-black active:bg-black hover:bg-blue-200" ><button className="p-6" onClick={(some:any) => (setWhichPage(1))}>Sub-In</button></div>
+                        <div className="bg-blue-700 text-base border-2 border-neon-yellow active:bg-black hover:bg-blue-200" ><button className="p-6" onClick={(some:any) => (setWhichPage(1))}>Sub-In</button></div>
                     </Link>
                     <Link href="sub-out">
-                        <div className="bg-red-700 text-base rounded-md border-2 border-black active:bg-black hover:bg-red-200"><button className="p-6" onClick={(some:any) => (setWhichPage(2))}>Sub-Out </button></div>
+                        <div className="bg-red-700 text-base border-2 border-neon-yellow active:bg-black hover:bg-red-200"><button className="p-6" onClick={(some:any) => (setWhichPage(2))}>Sub-Out </button></div>
                     </Link>
                     <Link href="availability">
-                        <div className="bg-green-700 text-base rounded-md border-2 border-black active:bg-black hover:bg-green-200"><button className="p-6" onClick={(some:any) => (setWhichPage(3))}>Availability </button></div>
+                        <div className="bg-green-700 text-base border-2 border-neon-yellow active:bg-black hover:bg-green-200"><button className="p-6" onClick={(some:any) => (setWhichPage(3))}>Availability </button></div>
                     </Link>
                 </div>
                     
@@ -40,13 +40,13 @@ export const Component = () => {
                 <div className ="flex justify-center w-screen fixed bg-neon-yellow">
                     <h1 className="fixed left-2 top-3">Safewalk</h1>
                     <Link href= "sub-in">
-                        <div className="bg-blue-700 text-base rounded-md border-2 border-black hover:bg-blue-200 active:bg-black" ><button className="p-6" onClick={(some:any) => (setWhichPage(1))}>Sub-In</button></div>
+                        <div className="bg-blue-700 text-base border-2 border-neon-yellow hover:bg-blue-200 active:bg-black" ><button className="p-6" onClick={(some:any) => (setWhichPage(1))}>Sub-In</button></div>
                     </Link>
                     <Link href="sub-out">
-                        <div className="bg-red-200 text-base rounded-md border-2  border-black active:bg-black hover:bg-red-700"><button className="p-6" onClick={(some:any) => (setWhichPage(2))}>Sub-Out </button></div>
+                        <div className="bg-red-200 text-base border-2  border-neon-yellow active:bg-black hover:bg-red-700"><button className="p-6" onClick={(some:any) => (setWhichPage(2))}>Sub-Out </button></div>
                     </Link>
                     <Link href="availability">
-                        <div className="bg-green-200 text-base rounded-md border-2 border-black active:bg-black hover:bg-green-700"><button className="p-6" onClick={(some:any) => (setWhichPage(3))}>Availability </button></div>
+                        <div className="bg-green-200 text-base border-2 border-neon-yellow active:bg-black hover:bg-green-700"><button className="p-6" onClick={(some:any) => (setWhichPage(3))}>Availability </button></div>
                     </Link>
                 </div>
                     
@@ -60,13 +60,13 @@ export const Component = () => {
                 <div className ="flex justify-center w-screen fixed bg-neon-yellow">
                     <h1 className="fixed left-2 top-3">Safewalk</h1>
                     <Link href= "sub-in">
-                        <div className="bg-blue-200 text-base rounded-md border-2 border-black active:bg-black hover:bg-blue-700" ><button className="p-6" onClick={(some:any) => (setWhichPage(1))}>Sub-In</button></div>
+                        <div className="bg-blue-200 text-base border-2 border-neon-yellow active:bg-black hover:bg-blue-700" ><button className="p-6" onClick={(some:any) => (setWhichPage(1))}>Sub-In</button></div>
                     </Link>
                     <Link href="sub-out">
-                        <div className="bg-red-700 text-base rounded-md border-2 border-black active:bg-black hover:bg-red-200"><button className="p-6" onClick={(some:any) => (setWhichPage(2))}>Sub-Out </button></div>
+                        <div className="bg-red-700 text-base border-2 border-neon-yellow active:bg-black hover:bg-red-200"><button className="p-6" onClick={(some:any) => (setWhichPage(2))}>Sub-Out </button></div>
                     </Link>
                     <Link href="availability">
-                        <div className="bg-green-200 text-base rounded-md border-2 border-black active:bg-black hover:bg-green-700"><button className="p-6" onClick={(some:any) => (setWhichPage(3))}>Availability </button></div>
+                        <div className="bg-green-200 text-base border-2 border-neon-yellow active:bg-black hover:bg-green-700"><button className="p-6" onClick={(some:any) => (setWhichPage(3))}>Availability </button></div>
                     </Link>
                 </div>
                     
@@ -80,13 +80,13 @@ export const Component = () => {
                 <div className ="flex justify-center w-screen fixed bg-neon-yellow">
                     <h1 className="fixed left-2 top-3">Safewalk</h1>
                     <Link href= "sub-in">
-                        <div className="bg-blue-200 text-base rounded-md border-2 border-black active:bg-black hover:bg-blue-700" ><button className="p-6" onClick={(some:any) => (setWhichPage(1))}>Sub-In</button></div>
+                        <div className="bg-blue-200 text-base border-2 border-neon-yellow active:bg-black hover:bg-blue-700" ><button className="p-6" onClick={(some:any) => (setWhichPage(1))}>Sub-In</button></div>
                     </Link>
                     <Link href="sub-out">
-                        <div className="bg-red-200 text-base rounded-md border-2 border-black active:bg-black hover:bg-red-700"><button className="p-6" onClick={(some:any) => (setWhichPage(2))}>Sub-Out </button></div>
+                        <div className="bg-red-200 text-base border-2 border-neon-yellow active:bg-black hover:bg-red-700"><button className="p-6" onClick={(some:any) => (setWhichPage(2))}>Sub-Out </button></div>
                     </Link>
                     <Link href="availability">
-                        <div className="bg-green-700 text-base rounded-md border-2 border-black active:bg-black hover:bg-green-200"><button className="p-6" onClick={(some:any) => (setWhichPage(3))}>Availability </button></div>
+                        <div className="bg-green-700 text-base border-2 border-neon-yellow active:bg-black hover:bg-green-200"><button className="p-6" onClick={(some:any) => (setWhichPage(3))}>Availability </button></div>
                     </Link>
                 </div>
                     
