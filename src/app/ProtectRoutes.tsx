90228bc9e744477d83d7a98e8b423690bcf3dc27
joshualbala <joshua_bala@brown.edu>
2025-01-04 14:42:46 -0500

    fixed reference error

--- src/app/ProtectRoutes.tsx
+++ src/app/ProtectRoutes.tsx
@@ -5,19 +5,16 @@ import { useEffect } from "react";
 export const protectRoute = () =>{
      const router = useRouter();
       
-        // useEffect(() => {
-        //   if (!getAuth().currentUser) {
-        //     router.push("/sign-in");
-        //   } 
-        // }, []);
-      try{
-        getAuth().onAuthStateChanged(function(user) {
-          //console.log("get auth exists")
-          if (!user) {
-            router.push("/sign-in");
-          } 
-        });
-    }catch(e){
-      router.push("/sign-in");
-    }
+        useEffect(() => {
+          try{
+            getAuth().onAuthStateChanged(function(user) {
+              //console.log("get auth exists")
+              if (!user) {
+                router.push("/sign-in");
+              } 
+            });
+          }catch(e){
+          router.push("/sign-in");
+        }
+        }, []);
 }
